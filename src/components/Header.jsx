fa7020409f0f185ed1a666c0f37ed1454d9718dc
muhrafifrbn <rafifrabbani041104@gmail.com>
2024-11-13 22:15:21 +0700

    merubah warna tampilan

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
-    <header className=" bg-orange-500 flex justify-center">
-      <div className="bg-sky-950 w-[70%] lg:w-1/2 text-white   h-10 lg:h-12  flex justify-center items-center font-serif font-extrabold lg:text-2xl text-md rounded-b-full">
+    <header className=" header flex justify-center">
+      <div className="background w-[70%] lg:w-1/2 text-white   h-10 lg:h-12  flex justify-center items-center font-serif font-extrabold lg:text-2xl text-md rounded-b-full">
         <h1>My Personal Notes</h1>
       </div>
     </header>
